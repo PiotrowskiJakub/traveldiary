ee02851c71a08ac7741933af11ec9c3c9c344ff4
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-03-09 13:20:46 +0100

    edited destination creation

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -5,6 +5,11 @@ class DestinationsController < ApplicationController
   # GET /destinations.json
   def index
     @destinations = Destination.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @destinations }
+    end
   end
 
   # GET /destinations/1
@@ -14,17 +19,21 @@ class DestinationsController < ApplicationController
 
   # GET /destinations/new
   def new
+    @trip  = Trip.find(params[:trip_id])
     @destination = Destination.new
   end
 
   # GET /destinations/1/edit
   def edit
+    @trip = Trip.find(params[:trip_id])
+    @destination = Destination.find(params[:id])
   end
 
   # POST /destinations
   # POST /destinations.json
   def create
-    @destination = Destination.new(destination_params)
+    @trip = Trip.find(params[:trip_id])
+    @destination = @trip.destinations.new(destination_params)
 
     respond_to do |format|
       if @destination.save
